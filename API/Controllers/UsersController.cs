ce7867fc9c20fa27c0efda0043ba293791f7596a
manjup-git <parvathym@outlook.com>
2024-04-18 01:41:15 -0400

    end of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Design.Serialization;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet(Name = "Users")]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return _context.Users.ToList();
+    }
+
+    [HttpGet("{id}/{UserName}")]
+    public ActionResult<AppUser> GetUser(int id, string UserName)
+    {
+        var user =_context.Users
+                    .Where(user => user.Id == id && user.UserName == UserName)
+                    .FirstOrDefault();
+        return user;
+    }
+     [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user =_context.Users.Find(id);
+        return user;
+    }
+}
